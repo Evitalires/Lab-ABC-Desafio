eee8b2975ed31b063f4b894e532463288269c106
Evitalires <evitalires@gmail.com>
2025-11-11 16:46:58 -0500

    creando la funcionalidad

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-import { abecedario } from "./scripts"
+// import { abecedario } from "./scripts"
+const abecedario = ["a", "b", "c"];
+let position = 0;
 
-console.log(abecedario);
-//["a", "b", "c"]
+console.log(abecedario); // ["a", "b", "c"]
 
-//let url = `/imagenes/${letra}.jpg`
+const imagenLetra = document.getElementById("imagen-letra")
+const letraAnterior = document.getElementById("letra-anterior");
+const letraSiguiente = document.getElementById("letra-siguiente");
+
+// ✅ Llamamos correctamente las funciones
+letraAnterior.addEventListener("click", () => cambiarLetra("prev"));
+letraSiguiente.addEventListener("click", () => cambiarLetra("next"));
+
+function cambiarLetra(type) {
+  const letra = document.getElementById("letra");
+
+  if (type === "next" && position < abecedario.length - 1) {
+    position += 1;
+  } else if (type === "prev" && position > 0) {
+    position -= 1;
+  }
+
+  letra.innerText = abecedario[position];
+  imagenLetra.style.backgroundImage = `url('/imagenes/${abecedario[position]}.png')`
+
+  console.log("Posición:", position);
+  console.log("Letra actual:", letra.innerText);
+}
